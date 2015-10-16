bf835c579917a227e8410f501f81e2c058d2bb2d
Michael <cerealean@me.com>
2015-10-16 13:41:42 -0500

    Initial Commit

---
+++ Cerulean.Lib/Units/Unit.cs
@@ -0,0 +1,12 @@
+﻿namespace Cerulean.Lib.Units
+{
+    public abstract class Unit
+    {
+        public int Health { get; private set; }
+        public int Armor { get; private set; }
+        public int AttackPower { get; private set; }
+
+        public abstract void Attack();
+        public abstract void TakeDamage();
+    }
+}
---
+++ Cerulean/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cerulean.Startup))]
+namespace Cerulean
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
